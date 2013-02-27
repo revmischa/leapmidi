31a2be9fae7366ac5155b9e27841c6cbe2a1ae1b
Mischa S <revmischa@cpan.org>
2013-02-26 19:46:23 -0800

    basic OSX midi output

--- leapmidi/MIDIListener.cpp
+++ leapmidi/MIDIListener.cpp
@@ -17,6 +17,9 @@ Listener::Listener() {
     BallGesturePtr ballGesture = make_shared<BallGesture>();
     GesturePtr ballGestureBasePtr = dynamic_pointer_cast<Gesture>(ballGesture);
     _gestureRecognizers.push_back(ballGestureBasePtr);
+    
+    midiPacketList = NULL;
+    createDevice();
 }
 
 void Listener::onFrame(const Leap::Controller &controller) {    
@@ -47,10 +50,71 @@ void Listener::onFrame(const Leap::Controller &controller) {
 // do something productive with these in your application's Listener subclass
 void Listener::onGestureRecognized(const Leap::Controller &controller, GesturePtr gesture) {}
 void Listener::onControlUpdated(const Leap::Controller &controller, GesturePtr gesture, ControlPtr control) {
-    cout << "recognized control index " << control->controlIndex()
-    << " (" << control->description() << ")"
-    << ", raw value: "
-    << control->rawValue() << " mapped value: " << control->mappedValue() << endl;
+    midi_control_index ctrlIdx = control->controlIndex();
+    midi_control_value ctrlVal = control->mappedValue();
+    
+    cout << "Recognized control index " << ctrlIdx
+         << " (" << control->description() << ")"
+         << ", raw value: "
+         << control->rawValue() << " mapped value: " << ctrlVal << endl;
+    
+    assert(ctrlIdx < 120);
+    assert(ctrlVal <= 127);
+    
+    // build midi packet
+    u_int8_t packetOut[3];
+    packetOut[0] = 0xB0;     // channel
+    packetOut[1] = ctrlIdx;  // controller number
+    packetOut[2] = ctrlVal;  // controller value
+    
+    int z;
+    printf("Sending MIDI packet: ");
+    for (z = 0; z < 3; z++)
+    {
+        if (z > 0) printf(":");
+        printf("%02X", packetOut[z]);
+    }
+    printf("\n");
+    
+    
+    // add packet to packet list
+    initPacketList();
+    curPacket = MIDIPacketListAdd(midiPacketList, packetListSize, curPacket, 0, 3, packetOut);
+    if (! curPacket) {
+        std::cerr << "Buffer overrun on midi packet list\n";
+        exit(1);
+    }
+    OSStatus res = MIDIReceived(deviceEndpoint, midiPacketList);
+    if (res)
+        fatal("MIDIReceived() failed");
+}
+
+void Listener::initPacketList() {
+    if (midiPacketList) {
+        free(midiPacketList);
+        midiPacketList = NULL;
+    }
+    
+    midiPacketList = (MIDIPacketList *)malloc(packetListSize * sizeof(char));
+    curPacket = MIDIPacketListInit(midiPacketList);
 }
     
+void Listener::createDevice() {
+    OSStatus result;
+    
+    result = MIDIClientCreate(CFSTR("LeapMIDIX"), NULL, NULL, &deviceClient);
+    if (result)
+        fatal("Failed to create MIDI client");
+    
+    result = MIDISourceCreate(deviceClient, CFSTR("LeapMIDIX Control"), &deviceEndpoint);
+    if (result)
+        fatal("Failed to create MIDI source");
+    
+}
+
+void Listener::fatal(const char *msg) {
+    cerr << "Fatal error: " << msg << endl;
+    exit(1);
+}
+
 }
